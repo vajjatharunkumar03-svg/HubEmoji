d58c721440a0b28a912dbb18dcb7cdd92256a417
vajjatharunkumar03-svg <vajjatharunkumar03@gmail.com>
2026-07-21 15:51:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const toast = document.getElementById("copy-toast");
+document.querySelectorAll(".emoji-card").forEach(card => {
+    card.addEventListener("click", function(e){
+        // Get only the first text node (emoji/kaomoji)
+        let emoji = this.childNodes[0].textContent.trim();
+        navigator.clipboard.writeText(emoji).catch(() => {
+    const input = document.createElement("textarea");
+    input.value = emoji;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+});
+        toast.style.left = e.clientX + "px";
+        toast.style.top = (e.clientY - 20) + "px";
+        toast.classList.add("show");
+        setTimeout(() => {
+            toast.classList.remove("show");
+        },1000);
+    });
+});
+const search = document.getElementById("search");
+const searchBtn = document.getElementById("searchBtn");
+function searchEmojis(){
+    let value = search.value.toLowerCase();
+    let cards = document.querySelectorAll(".emoji-card");
+    cards.forEach(function(card){
+        let text = (card.dataset.name || "").toLowerCase();
+        if(text.includes(value)){
+            card.style.display="flex";
+        }else{
+            card.style.display="none";
+        }
+    });
+}
+searchBtn.addEventListener("click", searchEmojis);
+search.addEventListener("keypress", function(e){
+    if(e.key==="Enter"){
+        searchEmojis();
+
+    }
+
+});
+
+// ==========================
+// CATEGORY BUTTONS
+// ==========================
+
